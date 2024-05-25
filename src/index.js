5839365b0ae1b47fa88dcf37ac561a593a9b1ea4
sadid369 <sadid.jones@gmail.com>
2024-05-26 03:47:17 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 // require('dotenv').config({ path: './env' });
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 dotenv.config({ path: './env' });
-connectDB();
+connectDB().then(() => {
+
+    app.listen(process.env.PORT || 7000, () => {
+        console.log(`Server is running PORT ${process.env.PORT || 7000}`);
+    });
+
+}).catch((err) => {
+    console.log("Mongo bd connection failed !!!", err);
+});
 
 
 
