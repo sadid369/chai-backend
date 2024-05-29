1cf21eb9e3765c58496453c9cbd5fd3c5d911b8e
sadid369 <sadid.jones@gmail.com>
2024-05-30 03:40:38 +0600

    part 12 completed

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,9 +1,17 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+    return async (req, res, next) => {
+        try {
+            await requestHandler(req, res, next);
+        } catch (err) {
+            next(err);
+        }
     };
-
 };
+// const asyncHandler = (requestHandler) => {
+//     return (req, res, next) => {
+//         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//     };
+// };
 
 
 export { asyncHandler };
