197b191b0d7cff93256456a6055bbc1ee37eb57c
sadid369 <sadid.jones@gmail.com>
2024-06-01 03:51:35 +0600

    chapter 14 completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,12 +24,17 @@ const registerUser = asyncHandler(async (req, res) => {
     }
     console.log(existedUser);
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
     if (!avatarLocalPath) {
         throw new ApiError(400, 'Avatar file is required');
     }
     const avatar = await uploadOnCloudinary(avatarLocalPath);
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
     const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
     console.log(req.files);
     if (!avatar) {
         throw new ApiError(400, 'Avatar file is required');
